e5bc733091efc98de69b388d23fa35a59ed6da85
juanan-fernandez <juanan.fernandez@gmail.com>
2023-02-19 23:16:08 +0100

    add another expand-collapse section

--- expand-collapse.js
+++ expand-collapse.js
@@ -1,9 +1,25 @@
 const dom = {
 	questionImage: document.getElementById('questionImage'),
+	btnExpandCollapse: document.getElementsByClassName('btnExpandCollapse'),
 };
 
-const showHide = () => {
-	alert('en la b');
-	console.log(dom.questionImage.innerHTML);
-	dom.questionImage.innerHTML = '<img src="/images/green_q.svg" />';
-};
+function showHide() {
+	const answerContainer = this.parentElement;
+	const answer = this.nextElementSibling;
+	answerContainer.classList.toggle('questionanswer__answer--expanded');
+	answer.classList.toggle('questionanswer__answer__content--expanded');
+	answer.style.maxHeight = answer.scrollHeight + 'px';
+	this.innerHTML =
+		answer.classList.length > 1
+			? '<img src="/images/close.png" />'
+			: '<img src="/images/expand.png" />';
+
+	//dom.questionImage.innerHTML = '<img src="/images/green_q.svg" />';
+	//dom.btnExpandCollapse.innerHTML =
+}
+
+(function configButtons() {
+	for (let i = 0; i < dom.btnExpandCollapse.length; i++) {
+		dom.btnExpandCollapse[i].addEventListener('click', showHide);
+	}
+})();
