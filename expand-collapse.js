b20eabef54e16f8fdfe625a14529c579b833ae39
juanan fernandez <juanan.fernandez@gmail.com>
2023-02-21 20:40:06 +0100

    add z-index property with js

--- expand-collapse.js
+++ expand-collapse.js
@@ -5,17 +5,19 @@ const dom = {
 
 function showHide() {
 	const answerContainer = this.parentElement;
+	const questionContainer = answerContainer.parentElement;
 	const answer = this.nextElementSibling;
+
 	answerContainer.classList.toggle('questionanswer__answer--expanded');
 	answer.classList.toggle('questionanswer__answer__content--expanded');
-	//answer.style.height = answer.scrollHeight + 'px';
+	questionContainer.style.zIndex = questionContainer.style.zIndex ? null : 10;
+
 	this.innerHTML =
 		answer.classList.length > 1
 			? '<img src="/images/close.png" />'
 			: '<img src="/images/expand.png" />';
 
-	//dom.questionImage.innerHTML = '<img src="/images/green_q.svg" />';
-	//dom.btnExpandCollapse.innerHTML =
+	dom.questionImage.innerHTML = '<img src="/images/green_q.svg" />';
 }
 
 (function configButtons() {
